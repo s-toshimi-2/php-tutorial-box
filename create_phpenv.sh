f692ec74771840eeb1b26fc7dc8b9b9b97ba8272
s-toshimi-2 <sionaion3@gmail.com>
2015-01-01 15:59:44 +0900

    provisionの順番変更＆/var/www/htmlにシンボリックリンクを貼るように修正

--- create_phpenv.sh
+++ create_phpenv.sh
@@ -8,7 +8,6 @@ if [ ! -e $HOME/.phpenv ]; then
     $HOME/phpenv/bin/phpenv-install.sh
     echo 'export PATH="/home/vagrant/.phpenv/bin:$PATH"' >> $HOME/.bashrc
     echo 'eval "$(phpenv init -)"' >> $HOME/.bashrc
-    . $HOME/.bashrc
 fi
 
 if [ -e $HOME/.phpenv ] && [ ! -e $HOME/.phpenv/plugins/php-build ]; then
@@ -18,11 +17,13 @@ if [ -e $HOME/.phpenv ] && [ ! -e $HOME/.phpenv/plugins/php-build ]; then
 fi
 
 if [ -e $HOME/.phpenv ] && [ ! -e $HOME/.phpenv/versions/system/libexec ]; then
-    mkdir -p $HOME/.phpenv/versions/system/libexec
-    sudo cp /etc/httpd/modules/libphp5.so /home/vagrant/.phpenv/versions/system/libexec
+    mkdir -p $HOME/.phpenv/versions/default/libexec
+    sudo cp /etc/httpd/modules/libphp5.so /home/vagrant/.phpenv/versions/default/libexec
 fi
 
 if [ -e $HOME/.phpenv ] && [ ! -e $HOME/.phpenv/plugins/phpenv-apache-version ]; then
     git clone https://github.com/garamon/phpenv-apache-version $HOME/.phpenv/plugins/phpenv-apache-version
     curl https://gist.githubusercontent.com/s-toshimi-2/1840adf4f39cc00105ae/raw/16a2afb9dc764657f27b4161036f9d27d1d22e55/phpenv-module-path.patch | patch -u $HOME/.phpenv/plugins/phpenv-apache-version/bin/rbenv-apache-version -
 fi
+
+echo "Restart your shell or execute 'source $HOME/.bashrc'"
